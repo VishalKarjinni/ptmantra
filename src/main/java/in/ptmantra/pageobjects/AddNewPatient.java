d70da3f4c5571d533a97f1069695dbb407f226cb
Vishal K <vishal.karjinni1998@gmail.com>
2022-06-07 13:15:40 +0530

    This is my thrid commit

---
+++ src/main/java/in/ptmantra/pageobjects/AddNewPatient.java
@@ -0,0 +1,5 @@
+package in.ptmantra.pageobjects;
+
+public class AddNewPatient {
+
+}
--- src/main/java/in/ptmantra/pageobjects/Dashboard.java
+++ src/main/java/in/ptmantra/pageobjects/Dashboard.java
@@ -3,10 +3,22 @@
  */
 package in.ptmantra.pageobjects;
 
+import java.util.Properties;
+
+import org.openqa.selenium.support.PageFactory;
+
+import in.ptmantra.actiondriver.Action;
+import in.ptmantra.base.BaseClass;
+
 /**
  * @author RSTPL087
  *
  */
-public class Dashboard {
-
+public class Dashboard extends BaseClass {
+	Action action=new Action();
+    Properties pro=new Properties();
+    
+    public Dashboard() {
+		PageFactory.initElements(getDriver(), this);
+	}
 }
